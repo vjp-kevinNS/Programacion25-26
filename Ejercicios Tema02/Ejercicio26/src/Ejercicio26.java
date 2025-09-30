16682a487a4b9aaf57c0bc822fd1a836ffccafbf
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-09-30 18:03:22 +0200

    Añadido ejercicio 26 sin terminar

---
+++ Ejercicios Tema02/Ejercicio26/src/Ejercicio26.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author alumno
+ */
+import java.util.Scanner;
+
+public class Ejercicio26 {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Por favor, introduzca un numero de 4 cifras: ");
+        
+        int cifraUno = sc.nextInt();
+        int cifraDos = sc.nextInt();
+        int cifraTres = sc.nextInt();
+        int cifraCuatro = sc.nextInt();
+        
+    }
+    
+}
