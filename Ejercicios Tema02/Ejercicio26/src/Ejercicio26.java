2b6de962e33e372093cf45820546729f88fb760d
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-10-03 19:19:58 +0200

    ejercicio 26 corregido

--- Ejercicios Tema02/Ejercicio26/src/Ejercicio26.java
+++ Ejercicios Tema02/Ejercicio26/src/Ejercicio26.java
@@ -12,10 +12,10 @@ public class Ejercicio26 {
         System.out.print("Por favor, introduzca un numero de 4 cifras: ");
         int numero = sc.nextInt();
         
-        int cifraUno = numero % 10;
-        int cifraDos = (numero / 10) % 10;
-        int cifraTres = (numero /100) % 10;
-        int cifraCuatro = (numero / 1000) % 10;
+        int cifraUno = (numero / 1000);
+        int cifraDos = (numero / 100) % 10;
+        int cifraTres = (numero /10) % 10;
+        int cifraCuatro = numero % 10;
         
         System.out.println (numero + " numero: "
                 + cifraUno + " cifra 1, "
