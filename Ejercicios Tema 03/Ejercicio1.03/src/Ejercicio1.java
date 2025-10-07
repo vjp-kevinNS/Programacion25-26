c53bd06b904d28c34f2dd85c7cf6a21ac1ab5d0f
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-10-07 15:36:25 +0200

    ejercicio 26

---
+++ Ejercicios Tema 03/Ejercicio1.03/src/Ejercicio1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+/**
+ *
+ * @author Kevin Nuñez Sanchez
+ */
+public class Ejercicio1 {
+             
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        
+        System.out.println("Por favor, introduce un número");
+        usuario = entrada.nextInt();
+        
+    if (usuario) = positivo;
+    System.out.println("numero positivo");        
+    }
+}
+  
+       
