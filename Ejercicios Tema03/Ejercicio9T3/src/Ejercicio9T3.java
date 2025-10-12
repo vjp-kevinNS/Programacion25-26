740938b2ed7ed1e2378a5cff94ab2608dab6c432
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-10-12 11:58:11 +0200

    Ejercicio9T3 sin completar

---
+++ Ejercicios Tema03/Ejercicio9T3/src/Ejercicio9T3.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author kevinNS
+ */
+import java.util.Scanner;
+public class Ejercicio9T3 {
+
+    public static void main(String[] args) {
+      Scanner entrada = new Scanner(System.in);
+        System.out.println("Por favor, introduzca el primer número"); //Se muestra el primer número
+      int numero1 = entrada.nextInt(); //Guardamos el primer número
+        System.out.println("Ahora, introduzca un segundo número"); //Se muestra el segundo número
+      int numero2 = entrada.nextInt(); //Guardamos el segundo número
+        System.out.println("Introduzca el tercer número"); //Se muestra el tercer número
+      int numero3 = entrada.nextInt(); //Guardamos el tercer número
+        System.out.println("Por último, introduzca un cuarto número"); //Se muestra el cuarto número
+      int numero4 = entrada.nextInt(); //Guardamos el cuarto número
+      
+      if (numero1 )
+    }
+    
+}
