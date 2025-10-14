e135663da7c057106d6aa4d6fe5a1d71646e1681
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-10-14 17:29:30 +0200

    Ejercicio13T3

--- Ejercicios Tema03/Ejercicio12T3/src/Ejercicio12T3.java
+++ Ejercicios Tema03/Ejercicio12T3/src/Ejercicio12T3.java
@@ -7,13 +7,13 @@ public class Ejercicio12T3 {
     public static void main(String[] args) {
     //Usando Do While crear un bucle que muestre los pares entre 11 y 133
         int i = 11; //Declaramos la variable i que en este caso es 11, por que queremos que empiece desde ahi
-        
+        //bucle do while
         do{ 
-            if (i % 2 == 0) {
+            if (i % 2 == 0) { //Si el número es par
             System.out.println(i);
             }
             i++; //para aumentar el contador
-        } while (i<=133);
+        } while (i<=133); //como queremos que sea hasta el 133 se quedaría ahi
     }
     
 }
---
+++ Ejercicios Tema03/Ejercicio13T3/src/Ejercicio13T3.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author KevinNS
+ */
+public class Ejercicio13T3 {
+
+    public static void main(String[] args) {
+        int i = 11; //Declaramos la variable por la que queremos que empiece
+        //Bucle while
+        
+       while (i<=133){ //como queremos que sea hasta el 133 se quedaría ahi
+            if (i % 2 == 0) { //Si el número es par
+            System.out.println(i);
+            }
+            i++; //para aumentar el contador
+        }
+    }
+}
+    
