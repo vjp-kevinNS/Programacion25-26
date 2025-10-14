19abfa985d1172fbd1584c459fa416f55f7b67bb
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-10-14 17:40:04 +0200

    Ejercicio14T3

---
+++ Ejercicios Tema03/Ejercicio14T3/src/Ejercicio14T3.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author KevinNS
+ */
+public class Ejercicio14T3 {
+
+    public static void main(String[] args) {
+        int i = 1; //Declaramos la variable i para utilizarla en el bucle
+        
+        for (i = 1; i<=100; i++){ //si es 0, hasta 100, mostrara los números pares
+            System.out.println(i * 2);
+        }
+    }
+     
+}
