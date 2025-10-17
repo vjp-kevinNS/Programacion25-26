3437342fac18b76cd48041d0d5cfdf281965c0f5
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-10-17 20:17:22 +0200

    Ejercicio18T3 sin completar

---
+++ Ejercicios Tema03/Ejercicio17T3/src/Ejercicio17T3.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author KevinNS
+ */
+import java.util.Scanner;
+public class Ejercicio17T3 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduzca un número"); //Le pedimos al usuario que introduzca un número para saber su raiz cuadrada
+        int num = entrada.nextInt(); //Guardamos el número
+        int resultado; //Declaramos la variable resultado para operar con ella
+        int numero = 0; //Declaramos la variable número para operar con ella
+        //primero realizamos un bucle while para que repita las veces que sea necesario
+        while (numero >=0){
+            System.out.println("Si el número es negativo será error");
+            numero = entrada.nextInt();
+        
+        if (numero >=0){
+            double resultado = Math.sqrt(num);
+        }
+        
+        
+    }
+        
+    }
+    
+}
