5c449e2e0f5fde692bb4a6041a48c5114f6adf0e
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-10-20 18:01:23 +0200

    Ejercicio28T3 sin terminar

--- Ejercicios Tema03/Ejercicio14T3/src/Ejercicio14T3.java
+++ Ejercicios Tema03/Ejercicio14T3/src/Ejercicio14T3.java
@@ -5,10 +5,10 @@
 public class Ejercicio14T3 {
 
     public static void main(String[] args) {
-        int i = 1; //Declaramos la variable i para utilizarla en el bucle
+        int i = 0; //Declaramos la variable i para utilizarla en el bucle
         
-        for (i = 1; i<=100; i++){ //si es 0, hasta 100, mostrara los números pares
-            System.out.println(i * 2);
+        for (i = 0; i<=100; i++){ //si es 0, hasta 100, mostrara los números pares
+            System.out.println(i++);
         }
     }
      
