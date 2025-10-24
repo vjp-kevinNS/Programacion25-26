2857383826d953f6e06d5ff763e335e9a3c68ff7
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-10-24 20:10:27 +0200

    Empiece de Ejercicio1T4

---
+++ Ejercicios tema04/Ejercicio1T4/src/Ejercicio1T4.java
@@ -0,0 +1,13 @@
+import Utilidades.Utilidades;
+/**
+ *
+ * @author KevinNS
+ */
+import java.util.Scanner;
+public class Ejercicio1T4 {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+    
+}
