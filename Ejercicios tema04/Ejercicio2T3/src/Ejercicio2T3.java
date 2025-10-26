7110837c2d355cf05eef8f68ed2b27da09d2bebf
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-10-26 12:31:13 +0100

    Ejercicio2T4

--- Ejercicios tema04/Ejercicio2T3/src/Ejercicio2T3.java
+++ Ejercicios tema04/Ejercicio2T3/src/Ejercicio2T3.java
@@ -16,9 +16,17 @@ public class Ejercicio2T3 {
         System.out.println("Ahora, introduzca un segundo número: "); //Pedimos el segundo número
         int num2 = entrada.nextInt();
         
-        // LLamamos a los métodos
-        int suma = Metodo1.operacionSuma(num1, num2);
-        int multiplicacion = Metodo2.multiplicacion(num1, num2);
+        //Realizamos un if else para que, depende de los números que se introduzcan, haga una operación u otra
+        if (num1 > 10){
+            int resultado = Metodo2.multiplicacion(num1, num2);
+            System.out.println("La operación que se realizó es una multiplicación: ");
+            System.out.println(num1 + "*" + num2 + "=" + resultado);
+        }else{
+            int resultado = Metodo1.operacionSuma(num1, num2);
+            System.out.println("La operación que se realizó es una suma: ");
+            System.out.println(num1 + "+" + num2 + "=" + resultado);
+        }
     }
+        
     
 }
