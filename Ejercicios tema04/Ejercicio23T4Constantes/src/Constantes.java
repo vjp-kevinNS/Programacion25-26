a6a3d6452cb1dc3e5f80cd7c8b6556e0d0f81f4a
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-11-04 19:00:00 +0100

    Ejercicio23T4 Constantes terminado

--- Ejercicios tema04/Ejercicio23T4Constantes/src/Constantes.java
+++ Ejercicios tema04/Ejercicio23T4Constantes/src/Constantes.java
@@ -1,11 +1,11 @@
-
 /**
- *
+ *Crear una clase llamada Constantes que
+ *contenga, únicamente, la declaración de 3 constantes estáticas
  * @author KevinNS
  */
 public class Constantes {
     //Aquí sólo declararemos las constantes
-    public final static String VELOCIDADLUZ = "299.792.458 metros por segundo";
-    public final static String CONSTANTEUNIVERSALGRAVITACION = "6.67430e-11";
-    public final static String CONSTANTEPLANCK = "6.62607015e-34";
+    public final static double VELOCIDADLUZ = 299792458; //metros por segundo
+    public final static double CONSTANTEUNIVERSALGRAVITACION = 6.67430e-11;
+    public final static double CONSTANTEPLANCK = 6.62607015e-34;
 }
--- Ejercicios tema04/Ejercicio23T4Constantes/src/Ejercicio23T4PruebaConstantes.java
+++ Ejercicios tema04/Ejercicio23T4Constantes/src/Ejercicio23T4PruebaConstantes.java
@@ -1,13 +1,16 @@
-
+import java.util.Scanner; //Importamos Scanner
 /**
- *
+ *crear otra clase que en su método main imprima por
+ *pantalla el valor de las 3 constantes declaradas.
  * @author KevinNS
  */
 public class Ejercicio23T4PruebaConstantes {
     
 
     public static void main(String[] args) {
-        
+        System.out.println("Velocidad de la luz: " + Constantes.VELOCIDADLUZ + " Metros por segundo ");
+        System.out.println("Constante universal de gravitación: " + Constantes.CONSTANTEUNIVERSALGRAVITACION + "N·m²/kg²");
+        System.out.println("Constante de Planck: " + Constantes.CONSTANTEPLANCK + "J·s");
     }
     
 }
