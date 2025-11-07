d61a24e4c3ed97ce558f7a940df186c05a2e171e
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-11-07 20:16:11 +0100

    Ejercicio1T5 Coche Iniciado

---
+++ Ejercicios tema04/Ejercicio1T5ClaseCoche/src/Coche.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author KevinNS
+ */
+public class Coche {
+    
+    //Creamos los atributos
+    private String marca;
+    private String modelo;
+    private String color;
+    public int velocidad;
+    public boolean motorEncendido;
+    
+    //Creamos el constructor por defecto
+}
---
+++ Ejercicios tema04/Ejercicio1T5ClaseCoche/src/Ejercicio1T5ClaseCoche.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author KevinNS
+ */
+public class Ejercicio1T5ClaseCoche {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
