9578ccb0237d98e16ea42f6ae7f7d104113e0edc
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-11-25 00:15:55 +0100

    Ejercicio23T5 sin terminar

--- Ejercicios Tema05/Ejercicio23T5Esquema/src/Autobus.java
+++ Ejercicios Tema05/Ejercicio23T5Esquema/src/Autobus.java
@@ -1,18 +1,40 @@
+
 /**
  *
  * @author KevinNS
  */
 public class Autobus extends Vehiculo {
-    
+
     //Creamos los atributos
     private int numeroPlazas;
-    
+
     //Creamos el constructor por defecto y el super
-    public Autobus(){
+    public Autobus() {
         super();
         this.numeroPlazas = 0;
     }
-    
-    //Creamos el constructor con los parametros y super
-    public Autobus()
+
+    //Creamos el constructor con los parametros y los heredados
+    public Autobus(int numeroPlazas, String matricula, String modelo, int potencia) {
+        super(matricula, modelo, potencia);
+        this.numeroPlazas = numeroPlazas;
+
+    }
+
+    //Creamos el getter y setter
+    public int getNumeroPlazas() {
+        return numeroPlazas;
+    }
+
+    public void setNumeroPlazas(int numeroPlazas) {
+        this.numeroPlazas = numeroPlazas;
+    }
+
+    //Creamos To String
+    @Override
+    public String toString() {
+        return super.toString() + ". AUTOBUS con número de plazas: "
+                + this.numeroPlazas;
+    }
+
 }
