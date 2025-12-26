19eddd3f3c0c458ef6700542e53e771aac19bbc4
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-12-26 08:50:39 +0100

    Ejercicio 32T2 terminado

--- REPASO PRIMERA EVALUACION/TEMA 2/Ejercicio32OptimoBilletes/src/Ejercicio32OptimoBilletes.java
+++ REPASO PRIMERA EVALUACION/TEMA 2/Ejercicio32OptimoBilletes/src/Ejercicio32OptimoBilletes.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Realiza un programa que dado un importe en euros nos indique número óptimo de
  * billetes de 50, 20, 10 y 5, así como la cantidad sobrante en monedas de 2 y 
@@ -14,6 +14,34 @@
 public class Ejercicio32OptimoBilletes {
 
     public static void main(String[] args) {
+        
+        //Pedimos al usuario que introduzca una cantidad
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduzca una cantidad");
+        int cantidad = entrada.nextInt();
+        
+        //Guardamos la cantidad y creamos otra variable para que no se pierda ese valor
+        int numero = cantidad;
+        
+        //Creamos las variables de los billetes y monedas
+        int billetes50 = cantidad  / 50;
+        cantidad = cantidad % 50;
+        int billetes20 = cantidad / 20;
+        cantidad = cantidad % 20;
+        int billetes10 = cantidad / 10;
+        cantidad = cantidad % 10;
+        int billetes5 = cantidad / 5;
+        cantidad = cantidad % 5;
+        int monedas2 = cantidad / 2;
+        cantidad = cantidad % 2;
+        int monedas1 = cantidad;
+        cantidad = cantidad % 1;
+        
+        //Imprimimos por pantalla el resultado
+        System.out.println(numero + " Se descomponen en " + billetes50 + 
+                " billetes de 50 " + billetes20 + " billetes de 20 " + billetes10 + 
+                " billetes de 10 " + billetes5 + " billetes de 5 " + monedas2 +
+                " monedas de 2 euros y " + monedas1 + " monedas de 1 euro.");
 
     }
 
