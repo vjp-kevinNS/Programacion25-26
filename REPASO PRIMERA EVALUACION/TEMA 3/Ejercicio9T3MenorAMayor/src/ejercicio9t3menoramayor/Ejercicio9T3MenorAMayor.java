ec4e04943744fbd446ddfe8ad155f98d6c59439f
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2025-12-30 09:07:45 +0100

    Ejercicio 9T3 terminado

--- REPASO PRIMERA EVALUACION/TEMA 3/Ejercicio9T3MenorAMayor/src/ejercicio9t3menoramayor/Ejercicio9T3MenorAMayor.java
+++ REPASO PRIMERA EVALUACION/TEMA 3/Ejercicio9T3MenorAMayor/src/ejercicio9t3menoramayor/Ejercicio9T3MenorAMayor.java
@@ -1,5 +1,5 @@
 package ejercicio9t3menoramayor;
-
+import java.util.Scanner;
 /**
  * Escribe un programa en JAVA en el que el usuario introduzca cuatro números
  * enteros y luego el programa los muestre por pantalla ordenados de forma
@@ -18,6 +18,42 @@ package ejercicio9t3menoramayor;
 public class Ejercicio9T3MenorAMayor {
 
     public static void main(String[] args) {
+        
+        //Pedimos al usuario que introduzca cuatro números
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Por favor, introduzca el primer número: ");
+        int num1 = entrada.nextInt();
+        System.out.println("Ahora, introduzca un segundo número: ");
+        int num2 = entrada.nextInt();
+        System.out.println("Introduzca el tercer número: ");
+        int num3 = entrada.nextInt();
+        System.out.println("por último, introduzca un cuarto número: ");
+        int num4 = entrada.nextInt();
+        
+        //Creamos una variable auxiliar
+        int aux;
+        
+        /**
+         * Ahora utilizamos if para hacer el método de la burbuja que consiste
+         * en ir comparando los números de dos en dos e ir ordenándolos entre ellos
+         */
+        
+        // Ciclo 1: Llevamos el número más grande al final (a la posición n4)
+        if (num1 > num2) { aux = num1; num1 = num2; num2 = aux; } // Intercambio si el primero es mayor [3]
+        if (num2 > num3) { aux = num2; num2 = num3; num3 = aux; }
+        if (num3 > num4) { aux = num3; num3 = num4; num4 = aux; }
+
+        // Ciclo 2: El más grande ya está en n4, ahora llevamos el siguiente a n3
+        if (num1 > num2) { aux = num1; num1 = num2; num2 = aux; }
+        if (num1 > num3) { aux = num2; num2 = num3; num3 = aux; }
+
+        // Ciclo 3: Ya solo nos queda asegurar el orden entre n1 y n2
+        if (num1 > num2) { aux = num1; num1 = num2; num2 = aux; }
+
+        //Imprimos el resultado por pantalla
+        
+        System.out.println("El orden de los números introducidos es el " + 
+                           num1 + " - " + num2 + " - " + num3 + " - " + num4);
 
     }
 
