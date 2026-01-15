844389f5145abda8939d25854dd8f91587545425
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2026-01-15 20:42:00 +0100

    Ejercicio 10 T7 Nada echo aún

---
+++ Ejercicios Tema07/Ejercicio10T7NumAleatorios1Y8/src/ejercicio10t7numaleatorios1y8/Ejercicio10T7NumAleatorios1Y8.java
@@ -0,0 +1,23 @@
+package ejercicio10t7numaleatorios1y8;
+
+/**
+ * Escribe un programa que contenga un método que rellene un vector de 10
+ * números enteros con números aleatorios entre 1 y 8, pudiendo contener
+ * elementos duplicados. 
+ * 
+ * • Otro método visualizará el array creado. Luego, otro método debe sustituir 
+ * cada valor repetido por 0. Para terminar, vuelve a visualizar el array ya modificado. 
+ * 
+ * • Ejemplo de ejecución: 
+ * Se han generado los siguientes números: 8 1 5 7 2 1 5 4 3 6 
+ * Sustituimos los elementos repetidos por un 0: 8 0 0 7 2 0 0 4 3 6
+ *
+ * @author KevinNS
+ */
+public class Ejercicio10T7NumAleatorios1Y8 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
