d71578bccc5b69b5c363511d9d6247a08f1a7dac
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2026-01-22 12:07:37 +0100

    Ejercicio 16 T7 terminado

--- Ejercicios Tema07/Ejercicio16T7NotasAlumnos/src/ejercicio16t7notasalumnos/Ejercicio16T7NotasAlumnos.java
+++ Ejercicios Tema07/Ejercicio16T7NotasAlumnos/src/ejercicio16t7notasalumnos/Ejercicio16T7NotasAlumnos.java
@@ -34,22 +34,160 @@ public class Ejercicio16T7NotasAlumnos {
         String[]nombreAlumnos = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
         String[]nombreAsignaturas = {"Lengua", "Mates", "Historia", "Física"};
         
-        // LLamamos a los métodos 
-        rellenarNotas(alumnos, nombreAlumnos, nombreAsignaturas);
+        /**
+         * Creamos un bucle do while con un try catch para hacer el menú.
+         * Utilizamos antes Scanner para pedirle la opción al usuario
+         */
+        Scanner entrada = new Scanner (System.in);
+        int opcion;
+        do {
+            System.out.println("----------------------------------------------");
+            System.out.println("---------------GESTION DE NOTAS---------------");
+            System.out.println("--------------1.Rellenar notas----------------");
+            System.out.println("--------------2.Mostrar notas-----------------");
+            System.out.println("----------3.Mostrar mejor alumno--------------");
+            System.out.println("------4.Mostrar alumno con mas suspensos------");
+            System.out.println("-------5.Mostrar asignatura más dificil-------");
+            System.out.println("-------------------6.SALIR--------------------");
+            System.out.println("---------------ELIJA UNA OPCION---------------");
+            System.out.println("----------------------------------------------");
+            opcion = entrada.nextInt();
+            
+            switch (opcion) {
+                case 1:
+                    rellenarNotas(alumnos, nombreAlumnos, nombreAsignaturas);
+                    break;
+                case 2:
+                    mostrarNotas(alumnos, nombreAlumnos, nombreAsignaturas);
+                    break;
+                case 3:
+                    mejorAlumno(alumnos, nombreAlumnos, nombreAsignaturas);
+                    break;
+                case 4:
+                    masSuspensos(alumnos, nombreAlumnos, nombreAsignaturas);
+                    break;
+                case 5:
+                    asignaturaDificil(alumnos, nombreAlumnos, nombreAsignaturas);
+                    break;
+                case 6:
+                    System.out.println("Saliendo del menú...");
+                    break;
+                default:
+                    System.out.println("Opción no valida...");
+            }
+            
+        } while (opcion != 6);
 
     }
     
-    public static void rellenarNotas(int[][]notas, String[]alumnos, String[]asignaturas){
-        Scanner entrada = new Scanner(System.in);
+    /**
+     * OPCION 1. Método que rellena las notas de los alumnos
+     * @param notas
+     * @param alumnos
+     * @param asignaturas 
+     */
+    public static void rellenarNotas(int[][]rellenar, String[]alumnos, String[]asignaturas){
+        System.out.println("INTRODUCIENDO NOTAS DE ALUMNOS");
         for (int i = 0; i < ALUMNOS; i++) {
-            System.out.println("El alumno " + (i + 1) + " -> ");
+            System.out.println("Introduciendo notas de: " + alumnos[i]);
             for (int j = 0; j < ASIGNATURAS; j++) {
-                System.out.println("Tiene éstas notas: " + asignaturas[j] + ": ");
-                notas[i][j] = entrada.nextInt();
+                rellenar[i][j] = (int) (Math.random() * 11);
             }
-            
+            System.out.println();
         }
     }
     
+    /**
+     * OPCION 2. Método que muestra las notas introducidas
+     * @param mostrar
+     * @param alumnos
+     * @param asignaturas 
+     */
+    public static void mostrarNotas(int[][]mostrar, String[]alumnos, String[]asignaturas){
+        System.out.println("MOSTRANDO NOTAS DE LOS ALUMNOS");
+        for (int i = 0; i < ALUMNOS; i++) {
+            System.out.println(alumnos[i] + " -> ");
+            for (int j = 0; j < ASIGNATURAS; j++) {
+                System.out.print(asignaturas[j] + ": " + mostrar[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+    
+    /**
+     * OPCION 3. Método que muestra el alumno con la nota media más alta
+     * @param mejor
+     * @param alumno
+     * @param asignatura 
+     */
+    public static void mejorAlumno(int[][]mejor, String[]alumno, String[]asignatura){
+        int maxMedia = -1; // Inicializamos asi para que cualquier media sea mayor
+        int indiceMejor = 0;
+        
+        for (int i = 0; i < ALUMNOS; i++) {
+            int suma = 0;
+            for (int j = 0; j < ASIGNATURAS; j++) {
+                suma += mejor[i][j]; // Sumamos las 4 notas del alumno   
+            }
+            int media = (int)suma / ASIGNATURAS; // Calculamos la media
+            if(media > maxMedia){
+                maxMedia = media;
+                indiceMejor = i; // Guardamos al mejor hasta ahora
+            }
+        }
+        System.out.println("El mejor alumno es: " + alumno[indiceMejor] + " con un: " 
+                + maxMedia + " de media total");
+    }
+    
+    /**
+     * OPCION 4. Método que muestra el alumno con más suspensos (nota de menos de 5)
+     * @param suspenso
+     * @param alumno
+     * @param asignatura 
+     */
+    public static void masSuspensos(int[][]suspenso, String[]alumno, String[]asignatura){
+        int maxSuspensos = -1; // Inicializamos asi para que cualquier media sea mayor
+        int indiceAlumno = 0;
+        
+        for (int i = 0; i < ALUMNOS; i++) {
+            int contador = 0;
+            for (int j = 0; j < ASIGNATURAS; j++) {
+                if (suspenso[i][j] < 5) contador++; // Si la nota es menor de 5, sumamos uno
+        }
+            if (contador > maxSuspensos) {
+                maxSuspensos = contador;
+                indiceAlumno = i;
+            }
+                
+            }
+        System.out.println("El alumno con mas suspensos es: " + alumno[indiceAlumno] + " que tiene " 
+                + maxSuspensos + " suspensos");
+    }
+    
+    /**
+     * OPCION 5. Método que nos muestra la asignatura más dificil sabiendo el
+     * total de suspensos
+     * @param dificil
+     * @param alumno
+     * @param asignatura 
+     */
+    public static void asignaturaDificil(int[][]dificil, String[]alumno, String[]asignatura){
+        int minMedia = 11; // Inicializamos alto para buscar el valor más bajo
+        int indiceAsig = 0;
+        
+        for (int j = 0; j < ASIGNATURAS; j++) { // Cambiamos el orden. Primero columnas
+            int suma = 0;
+            for (int i = 0; i < ALUMNOS; i++) {
+                suma += dificil[i][j]; // Sumamos las 4 notas de todos los alumnos   
+            }
+            int media = (int)suma / ALUMNOS; // Calculamos la media
+            if(media < minMedia){
+                minMedia = media;
+                indiceAsig = j;
+            }
+        }
+        System.out.println("La asignatura más dificil es: " + asignatura[indiceAsig] 
+                + " (Total: " + minMedia + ")");
+    }
 
 }
