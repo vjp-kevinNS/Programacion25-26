6a6b9fce25d9e1c60e3eb0432d3690bb9e2949fc
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2026-01-26 13:54:26 +0100

    Ejercicio 20T7 vacio

---
+++ Ejercicios Tema07/Ejercicio20T7TresEnRaya/src/ejercicio20t7tresenraya/Ejercicio20T7TresEnRaya.java
@@ -0,0 +1,31 @@
+package ejercicio20t7tresenraya;
+import java.util.Scanner; // Importamos Scanner
+/**
+ * Implementa en JAVA el juego del 3 en Raya mediante métodos estáticos. Para
+ * ello tendremos:
+ * 
+ * • Un método que se encargará de dibujar el tablero con las fichas que ya 
+ * hayan sido colocadas.
+ * 
+ * • Otro método para pedirle al jugador la posición donde colocar la ficha. 
+ * 
+ * • Un método permitirá que el ordenador coloque una ficha al azar.
+ * 
+ * • Otro método comprobará si el jugador que acaba de mover ha ganado la partida. 
+ * 
+ * • Por último, un método comprobará si la partida a terminado en empate 
+ *
+ * @author KevinNS
+ */
+public class Ejercicio20T7TresEnRaya {
+
+    public static void main(String[] args) {
+        
+        // Creamos una matriz que será un tablero 3 x 3 y tambíen añadimos Scanner
+        Scanner entrada = new Scanner (System.in);
+        char[][]tablero = new char[3][3];
+
+    }
+
+}
+
