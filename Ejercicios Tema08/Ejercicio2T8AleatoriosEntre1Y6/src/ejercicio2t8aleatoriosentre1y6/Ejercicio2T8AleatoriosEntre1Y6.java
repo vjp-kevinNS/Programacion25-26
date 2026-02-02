64194cc6b65ee1020430c3820bceade222183750
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2026-02-02 23:06:31 +0100

    Update Ejercicio2T8AleatoriosEntre1Y6.java

--- Ejercicios Tema08/Ejercicio2T8AleatoriosEntre1Y6/src/ejercicio2t8aleatoriosentre1y6/Ejercicio2T8AleatoriosEntre1Y6.java
+++ Ejercicios Tema08/Ejercicio2T8AleatoriosEntre1Y6/src/ejercicio2t8aleatoriosentre1y6/Ejercicio2T8AleatoriosEntre1Y6.java
@@ -1,20 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicio2t8aleatoriosentre1y6;
 
 /**
+ * Escribir un programa en JAVA que contenga un método que rellena un array
+ * unidimensional (vector) de enteros ALEATORIOS entre el 1 y el 6, y luego,
+ * otro método, lo muestre por pantalla, utilizando el bucle for-each. 
+ * 
+ * • El programa también tendrá un método donde el usuario elegirá la longitud del
+ * array entre 1 y 10. Este método también controla que el usuario nos
+ * introduzca un número entero y no una letra (mediante excepciones). En caso de
+ * que el usuario introduzca el dato incorrecto se lo volveremos a pedir las
+ * veces que hagan falta.
  *
  * @author KevinNS
  */
 public class Ejercicio2T8AleatoriosEntre1Y6 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
 }
