09134e1325a7fd503cf0e1d882132e6427016a8e
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2026-02-13 00:20:43 +0100

    Ejercicio17T8 intentando hacerle

---
+++ Ejercicios Tema08/Ejercicio17T8TresEnRaya/src/ejercicio17T8TresEnRaya/Ejercicio17T8TresEnRaya.java
@@ -0,0 +1,33 @@
+package ejercicio17T8TresEnRaya;
+
+/**
+ * Implementa en JAVA el juego del 3 en Raya orientado a objetos. Aunque se
+ * puede hacer de muchas maneras, esta es la que se propone en este caso:
+ * 
+ * • Una claseTablero, que contendrá un array de 3x3 de objetos de la clase Ficha. 
+ * 
+ * • Una clase Casilla, que contendrá un atributo ficha de tipo String (“O” o “X”)
+ * y otro atributo de tipo booleano llamado ocupada. 
+ * 
+ * • Un método de la clase Tablero se encargará de dibujar el tablero con las 
+ * fichas que ya hayan sido colocadas.
+ * 
+ * • Método estático para pedirle al jugador la posición donde colocar la ficha. 
+ * 
+ * • Método estático que permitirá al ordenador colocar una ficha al azar. 
+ * 
+ * • Método de la clase Tablero que comprobará si el jugador que acaba de mover 
+ * ha ganado la partida. 
+ * 
+ * • Por último, un método de la clase Tablero que comprobará si la partida ha 
+ * terminado en empate.
+ *
+ * @author KevinNS
+ */
+public class Ejercicio17T8TresEnRaya {
+
+    public static void main(String[] args) {
+
+    }
+
+}
