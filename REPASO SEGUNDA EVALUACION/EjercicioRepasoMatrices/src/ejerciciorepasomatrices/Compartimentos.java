ae9c0c1c1865eb3c92bb37ec21a8816391ae274f
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2026-03-05 20:33:41 +0100

    Ejercicio de repaso de matrices

--- REPASO SEGUNDA EVALUACION/EjercicioRepasoMatrices/src/ejerciciorepasomatrices/Compartimentos.java
+++ REPASO SEGUNDA EVALUACION/EjercicioRepasoMatrices/src/ejerciciorepasomatrices/Compartimentos.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejerciciorepasomatrices;
 
 /**
+ * String objetos, String material, float precio
  *
  * @author KevinNS
  */
 public class Compartimentos {
-    
+
+    // Creamos los atributos
+    private String objetos;
+    private String material;
+    private float precio;
+
+    // Creamos los constructores por defecto
+    public Compartimentos() {
+        this.objetos = "";
+        this.material = "";
+        this.precio = 0f;
+    }
+
+    // Creamos los constructores parametrizados
+    public Compartimentos(String objetos, String material, float precio) {
+        this.objetos = objetos;
+        this.material = material;
+        this.precio = precio;
+    }
+
+    // Creamos los getters y setters
+    public String getObjetos() {
+        return objetos;
+    }
+
+    public void setObjetos(String objetos) {
+        this.objetos = objetos;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    // Creamos To String
+    @Override
+    public String toString() {
+        return "Compartimentos " + " | Objetos: " + objetos + " | Material: " + material + " | Precio: " + precio;
+    }
+
 }
--- REPASO SEGUNDA EVALUACION/EjercicioRepasoMatrices/src/ejerciciorepasomatrices/EjercicioRepasoMatrices.java
+++ REPASO SEGUNDA EVALUACION/EjercicioRepasoMatrices/src/ejerciciorepasomatrices/EjercicioRepasoMatrices.java
@@ -5,30 +5,160 @@ import java.util.Scanner;
 
 /**
  * Crear una clase que se llame compartimentos Crear una matriz de
- * compartimentos que sea de 3x3 Crear un menu con la sisuignetes opciones:
+ * compartimentos que sea de 3x3
  *
- * 1-Mostrar todos los compartimentos (comprobando los nulos) 
- * 2-Colocar un compartimento en la posición indicada por el usuario (controlando
- * excepciones) 
- * 3-Eliminar un compartimento de la posición indicada(controlando excepciones) 
- * 4-Mostrar compartimento con mas valor 
- * 5-Mostrar compartimento con menos valor 
- * 6-Salir del programa
+ * Crear un menu con la siguientes opciones:
+ *
+ * 1-Mostrar todos los compartimentos (comprobando los nulos) 2-Colocar un
+ * compartimento en la posición indicada por el usuario (controlando
+ * excepciones) 3-Eliminar un compartimento de la posición indicada(controlando
+ * excepciones) 4-Mostrar compartimento con mas valor 5-Mostrar compartimento
+ * con menos valor 6-Salir del programa
  *
  * @author KevinNS
  */
 public class EjercicioRepasoMatrices {
 
+    /**
+     * Método que muestra las opciones del menú al usuario
+     */
     public static void mostrarMenu() {
         System.out.println("--------------------------------------------------");
         System.out.println("----------------#OPCIONES DEL MENÚ#---------------");
         System.out.println("-------------1.Mostrar compartimentos-------------");
         System.out.println("--------------2.Colocar compartimento-------------");
         System.out.println("-------------3.Eliminar compartimento-------------");
-        System.out.println("-----4.Mostrar compartimerntos con mas valor------");
-        System.out.println("----5.Mostrar compartimerntos con menos valor-----");
+        System.out.println("-----4.Mostrar compartimrntos con mas valor-------");
+        System.out.println("----5.Mostrar compartimentos con menos valor-----");
         System.out.println("--------------6.SALIR DEL PROGRAMA----------------");
         System.out.println("--------------------------------------------------");
+    }
+
+    /**
+     * Método que muestra los compartimentos
+     *
+     * @param compartimento
+     */
+    public static void mostrarCompartimentos(Compartimentos[][] compartimento) {
+
+        // Recorremos la matriz
+        for (int i = 0; i < compartimento.length; i++) {
+            for (int j = 0; j < compartimento[i].length; j++) {
+                // Controlamos que no esté vacía y mostramos
+                if (compartimento[i][j] != null) {
+                    System.out.println("Compartimento en la posicion " + i + " " + j
+                            + compartimento[i][j]);
+                }
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Método que pide al usuario la posición del objeto y todos sus datos.
+     * Luego nos muestras la información de la posición y el objeto.
+     *
+     * @param compartimentos
+     */
+    public static void colocarCompartimentos(Compartimentos[][] compartimentos) {
+
+        try {
+            Scanner entrada = new Scanner(System.in);
+
+            // Creamos las variables i y j para que el usuario indique en que posición quiere añadir el objeto
+            int i = 0;
+            int j = 0;
+
+            // Pedimos al usuario la posición
+            System.out.println("¿En qué posición quieres añadir el objeto(i)? :");
+            i = entrada.nextInt();
+            System.out.println("¿En qué posición quieres añadir el objeto(j)? :");
+            j = entrada.nextInt();
+            entrada.nextLine();
+
+            // Pedimos al usuario los datos de los objetos que irán dentro del compartimento
+            System.out.println("Introduce los datos de los objetos: ");
+            System.out.println("");
+
+            System.out.println("¿Qué objeto irá dentro del compartimento?: ");
+            String objetos = entrada.nextLine();
+            System.out.println("¿De que material está echo el objeto?: ");
+            String material = entrada.nextLine();
+            System.out.println("¿Què precio tiene?: ");
+            float precio = entrada.nextFloat();
+
+            // Creamos el objeto de compartimento con los datos del usuario
+            Compartimentos compartimento = new Compartimentos(objetos, material, precio);
+
+            // Añadimos el objeto a la matriz
+            compartimentos[i][j] = compartimento;
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Posición fuera de rango");
+        }
+
+    }
+
+    /**
+     * Método que elimina la posicion que indique el usuario
+     *
+     * @param compartimentos
+     */
+    public static void eliminarCompartimentos(Compartimentos[][] compartimentos) {
+        Scanner entrada = new Scanner(System.in);
+
+        // Creamos las variables i y j para que el usuario indique que posición borrar
+        int i = 0;
+        int j = 0;
+        System.out.println("Elimina un compartimento: ");
+        i = entrada.nextInt();
+        j = entrada.nextInt();
+
+        // Simplemente ponemos a null la posición indicada
+        compartimentos[i][j] = null;
+    }
+
+    /**
+     * Método que muestra el objeto del comaprtimento con más valor
+     *
+     * @param compartimentos
+     */
+    public static void mostrarConMasValor(Compartimentos[][] compartimentos) {
+        // Creamos un objeto de compartimento para ir guardando el compartimento de mayor valor 
+        Compartimentos compartimento = new Compartimentos("", "", 0);
+
+        // Recorremos la matriz
+        for (int i = 0; i < compartimentos.length; i++) {
+            for (int j = 0; j < compartimentos[i].length; j++) {
+                // Comparamos el valor de la matriz de compartimentos con  valor del objeto creado
+                if (compartimentos[i][j].getPrecio() > compartimento.getPrecio()) {
+                    compartimento = compartimentos[i][j]; // Si es mayor lo guardamos
+                }
+
+            }
+
+        }
+        System.out.println("El compartimento más valioso es: " + compartimento);
+    }
+
+    public static void mostrarConMenorValor(Compartimentos[][] compartimentos) {
+        // Creamos un objeto de compartimento para ir guardando el compartimento de menor valor 
+        Compartimentos compartimento = new Compartimentos("", "", 0);
+
+        // Recorremos la matriz
+        for (int i = 0; i < compartimentos.length; i++) {
+            for (int j = 0; j < compartimentos[i].length; j++) {
+                // Comparamos el valor de la matriz de compartimentos con valor del objeto creado
+                if (compartimentos[i][j].getPrecio() < compartimento.getPrecio()) {
+                    compartimento = compartimentos[i][j]; // Si es menor lo guardamos
+                }
+
+            }
+
+        }
+        System.out.println("El compartimento con menos valor es: " + compartimento);
     }
 
     public static void main(String[] args) {
@@ -43,23 +173,23 @@ public class EjercicioRepasoMatrices {
         do {
             mostrarMenu();
             opcion = entrada.nextInt();
-            
+
             try {
                 switch (opcion) {
                     case 1:
-
+                        mostrarCompartimentos(misCompartimentos);
                         break;
                     case 2:
-
+                        colocarCompartimentos(misCompartimentos);
                         break;
                     case 3:
-
+                        eliminarCompartimentos(misCompartimentos);
                         break;
                     case 4:
-
+                        mostrarConMasValor(misCompartimentos);
                         break;
                     case 5:
-
+                        mostrarConMenorValor(misCompartimentos);
                         break;
                     case 6:
                         System.out.println("Saliendo del programa...");
