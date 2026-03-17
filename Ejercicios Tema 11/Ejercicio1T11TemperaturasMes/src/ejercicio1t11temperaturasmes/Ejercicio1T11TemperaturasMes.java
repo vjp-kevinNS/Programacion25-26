3ba7b9284d4b2f55e9fa38e17d02cbfe833b2cf5
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2026-03-17 12:51:31 +0100

    Ejercicio 1T11, sin hacer nada aun

---
+++ Ejercicios Tema 11/Ejercicio1T11TemperaturasMes/src/ejercicio1t11temperaturasmes/Ejercicio1T11TemperaturasMes.java
@@ -0,0 +1,34 @@
+package ejercicio1t11temperaturasmes;
+
+/**
+ * Diseña programa que almacene las temperaturas medias de un mes. Para ello
+ * crearemos una HashMap de 31 posiciones relleno, el campo Clave, con el número
+ * del día, y el campo Valor con objetos de la clase Día.
+ *
+ * Hasta que el usuario pulse 5, mostrar un menú que nos permita: 
+ * 
+ * 1. Rellenar de forma aleatoria las temperaturas. Además el día 1 del mes no 
+ * tiene porqué ser un Lunes, será un día aleatorio de la semana. 
+ * 
+ * 2. Mostrar las temperaturas.
+ * Ejemplo: Jueves día 1: 40 grados, Viernes día 2: 35 grados, Sábado día 3: 38
+ * grados… 
+ * 
+ * 3. Visualizar la temperatura media del mes. 
+ * 
+ * 4. Día o días más calurosos del mes. Ejemplo: El día o días más calurosos 
+ * fueron: • El Jueves día 1 con 40 grados. • El Sábado día 18 con 40 grados. 
+ * 
+ * 5. Salir del programa.
+ * 
+ * • Fíjate que necesitarás un array con el nombre de los días de la semana.
+ *
+ * @author KevinNS
+ */
+public class Ejercicio1T11TemperaturasMes {
+
+    public static void main(String[] args) {
+
+    }
+
+}
