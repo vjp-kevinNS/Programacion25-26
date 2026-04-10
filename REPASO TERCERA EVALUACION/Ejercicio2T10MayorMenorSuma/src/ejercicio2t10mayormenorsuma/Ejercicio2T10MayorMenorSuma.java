9bf4ea404533aa1b86602170bf69490c64870a13
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2026-04-10 10:50:30 +0200

    Ejercicio 2T10 Iniciado

---
+++ REPASO TERCERA EVALUACION/Ejercicio2T10MayorMenorSuma/src/ejercicio2t10mayormenorsuma/Ejercicio2T10MayorMenorSuma.java
@@ -0,0 +1,23 @@
+package ejercicio2t10mayormenorsuma;
+
+/**
+ * Crea un programa que calcule el mayor, el menor y la suma de todos los
+ * elementos de una lista que contenga números enteros positivos. 
+ * 
+ * • El programa permitirá, mediante un método, que el usuario introduzca valores 
+ * hasta que introduzca un valor negativo. 
+ * 
+ * • Otro método visualizará los elementos de la lista 
+ * 
+ * • Por último, se mostrará el mayor, el menor y la suma de los elementos
+ * (implementado en 3 métodos independientes)
+ *
+ * @author KevinNS
+ */
+public class Ejercicio2T10MayorMenorSuma {
+
+    public static void main(String[] args) {
+
+    }
+
+}
