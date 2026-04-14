aede99cfcb8c22b40c6f9b37c26c7b22336d9d1c
vjp-kevinNS <knunnezs001@informatica.iesvalledeljerteplasencia.es>
2026-04-14 13:16:48 +0200

    Update EjercicioRepaso2.java

--- REPASO TERCERA EVALUACION/EjercicioRepaso2/src/ejerciciorepaso2/EjercicioRepaso2.java
+++ REPASO TERCERA EVALUACION/EjercicioRepaso2/src/ejerciciorepaso2/EjercicioRepaso2.java
@@ -44,25 +44,79 @@ public class EjercicioRepaso2 {
 
     }
 
-    public static void añadirHastaQueUsuarioDiga(ArrayList<Persona>lista){
-        Scanner entrada = new Scanner(System.in);
-        String respuesta; // Para la respuesta del usuario(si/no)
-        
-        while (lista.) {            
-            
+    public static void mostrarPersonas(ArrayList<Persona> lista) {
+        // Recorremos la lista
+        for (Persona persona : lista) {
+            System.out.println(persona + " ");
         }
-        
+    }
+
+    public static void buscarPersonaNombre(ArrayList<Persona> lista) {
+        Scanner entrada = new Scanner(System.in);
+
+        // Preguntamos al usuario el nombre a buscar
+        System.out.println("¿Qué nombre quires buscar?: ");
+        String nombreUsu = entrada.nextLine();
+
+        boolean encontrado = false; // Para indicar si se ha encontrado ese nombre
+        Persona auxiliar; // Para guardar a la persona si se ha encontrado
+
+        // Recorremos la lista
+        for (Persona persona : lista) {
+            if (persona.getNombre().equalsIgnoreCase(nombreUsu)) {
+                auxiliar = persona;
+                encontrado = true;
+            }
+        }
+
+        if (encontrado == false) {
+            System.out.println("No se ha encontrado a la persona con ese nombre");
+        } else {
+            System.out.println("Se ha encontrado a la persona con el nombre: " + nombreUsu);
+        }
+    }
+
+    public static void eliminarEdadMenor(ArrayList<Persona> lista) {
+        Scanner entrada = new Scanner(System.in);
+        int contador = 0; // Para indicar cuántas personas se han borrado
+
+        // Pedimos al usuario la edad a buscar
+        System.out.println("Introduce la edad para eliminar la que sea menor: ");
+        int edadUsu = entrada.nextInt();
+
+        // Recorremos la lista
+        for (Persona persona : lista) {
+            if (persona.getEdad() < edadUsu) {
+                lista.remove(persona);
+                contador++;
+            }
+        }
+        System.out.println("Personas borradas con éxito. Se han borrado: " + contador);
     }
     
-//    public static void mostrarPersonas(ArrayList<Persona> lista) {
-//        // Recorremos la lista
-//        for (Persona persona : lista) {
-//            System.out.println(persona + " ");
-//        }
-//    }
-    
-    public static void buscarPersonaNombre(ArrayList<Persona>lista){
-        
+
+    public static void clonarListaPorEdad(ArrayList<Persona> lista) {
+        Scanner entrada = new Scanner(System.in);
+        int contador = 0; // Para indicar cuántas personas de han añadido
+
+        // Creamos una lista vacía
+        ArrayList<Persona> nuevaLista = new ArrayList<>();
+
+        // Pedimos al usuario la edad para añadirla a la nueva lista
+        System.out.println("Introduce la edad: ");
+        int edadUsu = entrada.nextInt();
+
+        // Recorremos la lista
+        for (Persona persona : lista) {
+            if (persona.getEdad() == edadUsu) {
+                nuevaLista.add(persona);
+                contador++;
+            }
+
+        }
+        System.out.println("Se han añadido " + contador + " personas con la edad: " + edadUsu);
+        // Mostramos la nueva lista
+        mostrarPersonas(nuevaLista);
     }
 
     /**
@@ -71,10 +125,11 @@ public class EjercicioRepaso2 {
     public static void mostrarMenu() {
         System.out.println("GESTION DE PERSONAS");
         System.out.println("1. Añadir persona");
-        System.out.println("2. Añadir personas hasta que el usuario quiera");
-        System.out.println("3. Mostrar todas las personas");
-        System.out.println("4. Mostrar personas con mas edad");
-        System.out.println("5. Salir del menú...");
+        System.out.println("2. Mostrar todas las personas");
+        System.out.println("3. Buscar persona por nombre");
+        System.out.println("4. Eliminar persona con la edad menor indicada");
+        System.out.println("5. Clonar lista por edad");
+        System.out.println("6. Salir del menú...");
     }
 
     public static void main(String[] args) {
@@ -95,15 +150,18 @@ public class EjercicioRepaso2 {
                         añadirPersona(persona);
                         break;
                     case 2:
-
-                        break;
-                    case 3:
                         mostrarPersonas(persona);
                         break;
+                    case 3:
+                        buscarPersonaNombre(persona);
+                        break;
                     case 4:
-
+                        eliminarEdadMenor(persona);
                         break;
                     case 5:
+                        clonarListaPorEdad(persona);
+                        break;
+                    case 6:
                         System.out.println("Saliendo del programa...");
                         break;
                     default:
@@ -114,7 +172,7 @@ public class EjercicioRepaso2 {
                 System.out.println("Error. Has introducido una letra");
             }
 
-        } while (opcion != 5);
+        } while (opcion != 6);
     }
 
 }
